9e59166a24aaf943518f9abfb6e4681fb8f44957
Ronald Li <ronaldbrooklynli@gmail.com>
2017-11-29 22:37:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 
-function happyHolidaysTo(Rafaela) {
+function happyHolidaysTo('Rafaela') {
   return 'Happy holdiays, ${name}!'
 }
 
-function happyHolidayTo(Christmas, Rafaela) {
+function happyHolidayTo('Christmas', 'Rafaela') {
   return 'Happy ${holiday}, ${name}!'
 }
 
-function holidayCountDown(Christmas, 30) {
+function holidayCountDown('Christmas', 30) {
   return 'It\'s ${days} days until ${holiday}!'
 }
