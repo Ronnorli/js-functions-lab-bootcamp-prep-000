d8310c261f040bcdbde39b238acb8f9260e379b2
Ronald Li <ronaldbrooklynli@gmail.com>
2017-11-29 22:47:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return 'Happy Independence Day, you!'
 }
 
-function holidayCountdown('holiday', 'days') {
+function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}!"
 }
